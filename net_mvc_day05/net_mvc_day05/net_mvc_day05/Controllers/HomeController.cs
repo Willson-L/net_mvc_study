554b6519179bb9225c3b65dca79be37cc412e22f
Willson-L <50355855+Willson-L@users.noreply.github.com>
2020-02-02 22:19:57 +0800

    create controller Report_A

--- net_mvc_day05/net_mvc_day05/net_mvc_day05/Controllers/HomeController.cs
+++ net_mvc_day05/net_mvc_day05/net_mvc_day05/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using net_mvc_day05.Models;
 namespace net_mvc_day05.Controllers
 {
     public class HomeController : Controller
@@ -24,6 +24,21 @@ namespace net_mvc_day05.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult ReportList_A()
+        {
+            TCP_ethernet_ImmediatelyEntities db = new TCP_ethernet_ImmediatelyEntities();
+            List<Module_data> GetList()
+            {
+
+                List<Module_data> Module_dataList = db.Module_data.OrderBy(Module_data => Module_data.Module_id).ToList();
+                return Module_dataList;
+            }
+            Module_data NewData = new Module_data();
+            return View(GetList());
+        }
+        public ActionResult ReportList_B() {
             return View();
         }
     }
